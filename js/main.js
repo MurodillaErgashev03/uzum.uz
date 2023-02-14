534d69a24a055dac8ccd55c8d278c517a2bed3d6
MurodillaErgashev03 <ergashevmurodilla27@gmail.com>
2023-02-15 00:29:11 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 let elTop = findElement('#products-top-id');
 let elTopTemplate = findElement('#product-template');
+let elLangSelect = findElement("#language-select");
+
+let  lang = localStorage.getItem("lang");
+elLangSelect.value = lang; 
+
+if (lang === "uz") {
+    document.title = "Internet do'kon";
+}
+else if (lang === "eng") {
+    document.title = "e-commerce";
+}
+else if (lang === "ru") {
+    document.title = "Интернет магазин";
+}
+
+elLangSelect.addEventListener("change", () => {
+
+    let value = elLangSelect.value;
+
+    localStorage.setItem("lang", value);
+    lang = value;
+
+
+
+    if (lang === "uz") {
+        document.title = "Internet do'kon";
+    }
+    else if (lang === "eng") {
+        document.title = "e-commerce";
+    }
+    else if (lang === "ru") {
+        document.title = "Интернет магазин";
+    }
+
+
+})
+
 
 
 renderProducts(products, elTop);
